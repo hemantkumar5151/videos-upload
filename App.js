e705c0d725b56ef9a91f65b658746c2f88a11ef6
hemant <hemantsingh5151@gmail.com>
2020-06-08 15:25:42 +0530

    version-1

---
+++ App.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const path = require('path');
+require('dotenv').config({ path: './.env'});
+
+const checkAuth = require('./middleware/check-auth');
+const db = process.env.DATABASE.replace('<PASSWORD>',process.env.DATABASE_PASSWORD);
+mongoose.connect(db, {
+  useCreateIndex: true, 
+  useNewUrlParser: true, 
+  useUnifiedTopology: true});
+mongoose.Promise = global.Promise;
+
+app.use(morgan('dev'));
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.use('/api/videos', express.static('media/uploads'));
+
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static('../client/build'))
+
+  app.get('*',(req,res) => {
+    res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'))
+  })
+}
+// Routes
+app.use('/api/signIn', require('./routes/signIn'));
+app.use('/api/signUp', require('./routes/signUp'));
+app.use('/api/upload', checkAuth, require('./routes/upload'));
+app.use('/api/videoList', checkAuth, require('./routes/videoList'));
+
+module.exports = app;
