5d7eb487019fd84bb4ee7a7d08f2bd59fabf6905
hemant <hemantsingh5151@gmail.com>
2020-06-08 15:51:56 +0530

    version-1

--- App.js
+++ App.js
@@ -23,10 +23,10 @@ app.use(bodyParser.json());
 app.use('/api/videos', express.static('media/uploads'));
 
 if(process.env.NODE_ENV === 'production') {
-  app.use(express.static('../client/build'))
+  app.use(express.static('client/build'))
 
   app.get('*',(req,res) => {
-    res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'))
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   })
 }
 // Routes
